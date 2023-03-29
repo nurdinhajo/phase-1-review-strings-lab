2cea65cf31f079f6b783fa13c7e858a224185aa5
nuru <nurudiin222@gmail.com>
2023-03-29 16:53:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+//Test 1
+const currentUser = "Nuru"
+
+//Test 2
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+
+
+//Test 3
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()} !`
+
+//Test 4
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial} !`
